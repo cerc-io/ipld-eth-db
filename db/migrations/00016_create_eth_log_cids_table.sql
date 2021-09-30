562ee3e782e43cfd831bacfdd3df595d4c5def94
Arijit Das <arijitad.in@gmail.com>
2021-09-30 18:09:48 +0530

    Create a docker image with postgres DB and concise migration.

--- db/migrations/00016_create_eth_log_cids_table.sql
+++ db/migrations/00016_create_eth_log_cids_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE eth.log_cids (
     leaf_cid            TEXT NOT NULL,
     leaf_mh_key         TEXT NOT NULL REFERENCES public.blocks (key) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
     receipt_id          INTEGER NOT NULL REFERENCES eth.receipt_cids (id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
-    address             VARCHAR(66),
+    address             VARCHAR(66) NOT NULL,
     log_data            BYTEA,
     index               INTEGER NOT NULL,
     topic0              VARCHAR(66),
@@ -19,6 +19,7 @@ CREATE INDEX log_mh_index ON eth.log_cids USING btree (leaf_mh_key);
 CREATE INDEX log_cid_index ON  eth.log_cids USING btree (leaf_cid);
 
 CREATE INDEX log_rct_id_index ON eth.log_cids USING btree (receipt_id);
+
 --
 -- Name: log_topic0_index; Type: INDEX; Schema: eth; Owner: -
 --
---
+++ scripts/startup_script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Runs the db migrations
+set +x
+
+# Construct the connection string for postgres
+VDB_PG_CONNECT=postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOSTNAME:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable
+
+# Run the DB migrations
+echo "Connecting with: $VDB_PG_CONNECT"
+echo "Running database migrations"
+./goose -dir migrations/vulcanizedb postgres "$VDB_PG_CONNECT" up
+
+# If the db migrations ran without err
+if [[ $? -eq 0 ]]; then
+    echo "Migration process ran successfully"
+else
+    echo "Could not run migrations. Are the database details correct?"
+    exit 1
+fi
