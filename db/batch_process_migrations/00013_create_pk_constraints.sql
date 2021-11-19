cb4d0a12759c25f65de0bf89ff8fd94580551c4d
i-norden <iansnorden@gmail.com>
2021-11-18 18:53:55 -0600

    fix access list and log pks

--- db/batch_process_migrations/00013_create_pk_constraints.sql
+++ db/batch_process_migrations/00013_create_pk_constraints.sql
@@ -18,10 +18,10 @@ ALTER TABLE eth.receipt_cids
 ADD CONSTRAINT pk_eth_receipt_cids PRIMARY KEY (tx_id);
 
 ALTER TABLE eth.access_list_elements
-ADD CONSTRAINT pk_eth_access_list_elements PRIMARY KEY (tx_id);
+ADD CONSTRAINT pk_eth_access_list_elements PRIMARY KEY (tx_id, index);
 
 ALTER TABLE eth.log_cids
-ADD CONSTRAINT pk_eth_log_cids PRIMARY KEY (rct_id);
+ADD CONSTRAINT pk_eth_log_cids PRIMARY KEY (rct_id, index);
 
 ALTER TABLE eth.state_cids
 ADD CONSTRAINT pk_eth_state_cids PRIMARY KEY (header_id, state_path);
