a2c98550b7a89858d0ca5a6cf0c3f8f8cb3ee788
i-norden <iansnorden@gmail.com>
2022-01-10 12:06:13 -0600

    mig to upgrade schema version in db; mig to remove temporary indexes for logTrie fix

--- db/post_batch_processing_migrations/00017_update_db_version.sql
+++ db/post_batch_processing_migrations/00017_update_db_version.sql
@@ -4,3 +4,5 @@ ON CONFLICT (singleton) DO UPDATE SET (version, tstamp) = ('v3.0.0', NOW());
 
 -- +goose Down
 DELETE FROM public.db_version WHERE version = 'v3.0.0';
+INSERT INTO public.db_version (singleton, version) VALUES (true, 'v0.3.2')
+ON CONFLICT (singleton) DO UPDATE SET (version, tstamp) = ('v0.3.2', NOW());
