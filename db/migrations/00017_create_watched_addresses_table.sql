61a4cea80ba7e515d0142b97676f2d8984299dc5
prathamesh0 <prathamesh.musale0@gmail.com>
2022-01-18 14:55:04 +0530

    Update watched addresses table to track gap filling

--- db/migrations/00017_create_watched_addresses_table.sql
+++ db/migrations/00017_create_watched_addresses_table.sql
@@ -1,7 +1,9 @@
 -- +goose Up
 CREATE TABLE eth.watched_addresses (
-    address               VARCHAR(66) PRIMARY KEY,
-    added_at              BIGINT NOT NULL
+    address                 VARCHAR(66) PRIMARY KEY,
+    created_at              BIGINT NOT NULL,
+    watched_at              BIGINT NOT NULL,
+    last_filled_at          BIGINT NOT NULL DEFAULT 0
 );
 
 -- +goose Down
