3b4e097f002e21123418ef9be3c532dc2fb8f969
i-norden <iansnorden@gmail.com>
2022-04-12 21:12:03 -0500

    remove pre and post migrations sets

--- db/pre_batch_processing_migrations/00001_create_ipfs_blocks_table.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-CREATE TABLE IF NOT EXISTS public.blocks (
-    block_number BIGINT NOT NULL,
-    key TEXT NOT NULL,
-    data BYTEA NOT NULL
-);
-
--- +goose Down
-DROP TABLE public.blocks;
