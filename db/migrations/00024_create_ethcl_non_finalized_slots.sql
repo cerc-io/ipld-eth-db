d8de075fbf79d00ee772dd0dbadb707bead5f870
Abdul Rabbani <abdulrabbani00@gmail.com>
2022-04-29 09:57:02 -0400

    Fix typo

--- db/migrations/00024_create_ethcl_non_finalized_slots.sql
+++ db/migrations/00024_create_ethcl_non_finalized_slots.sql
@@ -3,7 +3,7 @@ CREATE TABLE ethcl.non_finalized_slots (
   "slot" bigint NOT NULL,
   "block_root" VARCHAR(66) UNIQUE,
   "state_root" VARCHAR(66) UNIQUE,
-  FOREIGN KEY (slot, block_root) REFERENCES ethcl.slot (slot, block_root) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+  FOREIGN KEY (slot, block_root) REFERENCES ethcl.slots (slot, block_root) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
   PRIMARY KEY (slot, block_root)
 );
 
