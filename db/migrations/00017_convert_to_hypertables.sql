92cc8fbea3e396635e111c41755cf017c2389eb5
i-norden <iansnorden@gmail.com>
2023-02-21 20:01:48 -0600

    remove access_list_elements table and associated indexes and hypertable functions

--- db/migrations/00017_convert_to_hypertables.sql
+++ db/migrations/00017_convert_to_hypertables.sql
@@ -6,7 +6,6 @@ SELECT create_hypertable('eth.transaction_cids', 'block_number', migrate_data =>
 SELECT create_hypertable('eth.receipt_cids', 'block_number', migrate_data => true, chunk_time_interval => 32768);
 SELECT create_hypertable('eth.state_cids', 'block_number', migrate_data => true, chunk_time_interval => 32768);
 SELECT create_hypertable('eth.storage_cids', 'block_number', migrate_data => true, chunk_time_interval => 32768);
-SELECT create_hypertable('eth.access_list_elements', 'block_number', migrate_data => true, chunk_time_interval => 32768);
 SELECT create_hypertable('eth.log_cids', 'block_number', migrate_data => true, chunk_time_interval => 32768);
 
 -- update version
@@ -20,7 +19,6 @@ INSERT INTO public.db_version (singleton, version) VALUES (true, 'v5.0.0')
 -- reversing conversion to hypertable requires migrating all data from every chunk back to a single table
 -- create new regular tables
 CREATE TABLE eth.log_cids_i (LIKE eth.log_cids INCLUDING ALL);
-CREATE TABLE eth.access_list_elements_i (LIKE eth.access_list_elements INCLUDING ALL);
 CREATE TABLE eth.storage_cids_i (LIKE eth.storage_cids INCLUDING ALL);
 CREATE TABLE eth.state_cids_i (LIKE eth.state_cids INCLUDING ALL);
 CREATE TABLE eth.receipt_cids_i (LIKE eth.receipt_cids INCLUDING ALL);
@@ -31,7 +29,6 @@ CREATE TABLE ipld.blocks_i (LIKE ipld.blocks INCLUDING ALL);
 
 -- migrate data
 INSERT INTO eth.log_cids_i (SELECT * FROM eth.log_cids);
-INSERT INTO eth.access_list_elements_i (SELECT * FROM eth.access_list_elements);
 INSERT INTO eth.storage_cids_i (SELECT * FROM eth.storage_cids);
 INSERT INTO eth.state_cids_i (SELECT * FROM eth.state_cids);
 INSERT INTO eth.receipt_cids_i (SELECT * FROM eth.receipt_cids);
@@ -42,7 +39,6 @@ INSERT INTO ipld.blocks_i (SELECT * FROM ipld.blocks);
 
 -- drop hypertables
 DROP TABLE eth.log_cids;
-DROP TABLE eth.access_list_elements;
 DROP TABLE eth.storage_cids;
 DROP TABLE eth.state_cids;
 DROP TABLE eth.receipt_cids;
@@ -53,7 +49,6 @@ DROP TABLE ipld.blocks;
 
 -- rename new tables
 ALTER TABLE eth.log_cids_i RENAME TO log_cids;
-ALTER TABLE eth.access_list_elements_i RENAME TO access_list_elements;
 ALTER TABLE eth.storage_cids_i RENAME TO storage_cids;
 ALTER TABLE eth.state_cids_i RENAME TO state_cids;
 ALTER TABLE eth.receipt_cids_i RENAME TO receipt_cids;
