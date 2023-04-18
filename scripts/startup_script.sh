050c1ae3dfd21bf4ad12cf24a62de233d74dd99b
Roy Crihfield <roy@manteia.ltd>
2023-04-18 20:39:22 +0800

    Startup script tweaks
    * forward goose command
    * don't idle migrations container

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Runs the db migrations
-set +x
+set -e
+
+# Default command is "goose up"
+if [[ $# -eq 0 ]]; then
+  set -- "up"
+fi
 
 # Construct the connection string for postgres
 VDB_PG_CONNECT=postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOSTNAME:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable
@@ -8,12 +13,11 @@ VDB_PG_CONNECT=postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOSTNAME
 # Run the DB migrations
 echo "Connecting with: $VDB_PG_CONNECT"
 echo "Running database migrations"
-./goose -dir migrations postgres "$VDB_PG_CONNECT" up
+./goose -dir migrations postgres "$VDB_PG_CONNECT" "$@"
 
 # If the db migrations ran without err
 if [[ $? -eq 0 ]]; then
     echo "Migration process ran successfully"
-    tail -f /dev/null
 else
     echo "Could not run migrations. Are the database details correct?"
     exit 1
