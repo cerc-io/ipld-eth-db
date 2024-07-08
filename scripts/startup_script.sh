825a0bc2352060ff8f9d973f2266049d35cf5067
Roy Crihfield <roy@manteia.ltd>
2024-07-08 17:48:16 +0800

    Simplify startup script

--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# Runs the db migrations
 set -e
 
 # Default command is "goose up"
@@ -11,14 +10,5 @@ fi
 VDB_PG_CONNECT=postgresql://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOSTNAME:$DATABASE_PORT/$DATABASE_NAME?sslmode=disable
 
 # Run the DB migrations
-echo "Connecting with: $VDB_PG_CONNECT"
-echo "Running database migrations"
-./goose -dir migrations postgres "$VDB_PG_CONNECT" "$@"
-
-# If the db migrations ran without err
-if [[ $? -eq 0 ]]; then
-    echo "Migration process ran successfully"
-else
-    echo "Could not run migrations. Are the database details correct?"
-    exit 1
-fi
+set -x
+exec ./goose -dir migrations postgres "$VDB_PG_CONNECT" "$@"
